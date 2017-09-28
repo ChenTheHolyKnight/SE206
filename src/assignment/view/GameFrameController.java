8e536d2d2d724b38af50dbe0da88113bdd821661
DweeDog <dweep.kapadia@gmail.com>
2017-09-29 04:53:00 +1300

    refactoring GameFrameController to suit new scene

--- src/assignment/view/GameFrameController.java
+++ src/assignment/view/GameFrameController.java
@@ -32,205 +32,338 @@ import javafx.scene.image.WritableImage;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.util.Duration;
+import sun.plugin.javascript.navig.Anchor;
+
+import java.io.WriteAbortedException;
 
 public class GameFrameController {
 
+	//private variables for FXML file
 	@FXML
 	private Pane _Pane;
+
 	@FXML
 	private ImageView _imageView;
+
 	@FXML
 	private ImageView _imageView1;
 
+	//buttons -> FXML file
 	@FXML
 	private Button _nextButton;
+
 	@FXML
 	private Button _reBtn;
+
 	@FXML
 	private Button _submitBtn;
+
 	@FXML
 	private Button _playBtn;
+
 	@FXML
 	private Button _backBtn;
+
+	//vars for TextFields
 	@FXML
 	private JFXTextField _playerAnswer;
+
 	@FXML
 	private JFXTextField _correctAnswer;
+
 	@FXML
 	private ImageView _correctnessImage;
+
 	@FXML
 	private Label _label;
+
 	@FXML
 	private AnchorPane _anchorPane;
 
 
-
+	//private variables for functionality
 	private Level _level;
 
 	private MainApp _mainApp;
 
+	//counter vars
 	private Counter _frameCounter;
 
 	private Counter _attemptCounter;
 
 	private Counter _scoreCounter;
-	
+
 	private Counter _recCounter;
 
+	//other vars
 	private Player _player;
 
-	private int _num=0;
-	
-	
-	
-	
-	private boolean _isPopOverShown=false;
+	private int _num = 0;
 
+	
+	
+	
+	//pop over is not shown currently
+	private boolean _isPopOverShown = false;
+
+	/**
+	 * initialize method for the GameFrame , initializes the counters and
+	 * sets the image graphics on the buttons also locks certain buttons which
+	 * can only be used after certain functionality has been attained
+	 * play and submit is locked if record hasn't been pressed
+	 */
 	@FXML
 	public void initialize() {
-			
-		_frameCounter=new Counter(10);
-		_attemptCounter=new Counter();
-		_scoreCounter=new Counter(10);
-		_recCounter=new Counter();
-		
-		Image image=new Image(getClass().getClassLoader().getResource("resources/gameImage.jpg").toString());
+
+		//initialize the counters which is are required
+		_frameCounter = new Counter(10);
+		_attemptCounter = new Counter();
+		_scoreCounter = new Counter(10);
+		_recCounter = new Counter();
+
+		//loading the game back view
+		Image image = new Image(getClass().getClassLoader().getResource("resources/gameImage.jpg").toString());
 		_imageView.setImage(image);
 
-		Image reImage=new Image(getClass().getClassLoader().getResource("resources/record2.png").toString());
+		//loading image icons on the buttons
+		Image reImage = new Image(getClass().getClassLoader().getResource("resources/record2.png").toString());
 		_reBtn.setGraphic(new ImageView(reImage));
-		Image playImage=new Image(getClass().getClassLoader().getResource("resources/play1.png").toString());
+
+		Image playImage = new Image(getClass().getClassLoader().getResource("resources/play1.png").toString());
 		_playBtn.setGraphic(new ImageView(playImage));
-		Image subImage=new Image(getClass().getClassLoader().getResource("resources/submit.png").toString());
+
+		Image subImage = new Image(getClass().getClassLoader().getResource("resources/submit.png").toString());
 		_submitBtn.setGraphic(new ImageView(subImage));
-			
+
+		//locks the base buttons before the record button is pressed
 		lockSomeBtns();
 	}
 
+	/**
+	 * the handle next button handles the changing of the game frame which is based
+	 * on creating a numeric and adding that to the label whenever the next button.
+	 */
 	@FXML
 	public void handleNextButton() {
-		_recCounter=new Counter();
-		Image reimage=new Image(getClass().getClassLoader().getResource("resources/record1.png").toString());
-		_reBtn.setGraphic(new ImageView(reimage));		
+		//initializing the record counter
+		_recCounter = new Counter();
+
+		//setting the record button graphic
+		Image reimage = new Image(getClass().getClassLoader().getResource("resources/record1.png").toString());
+
+		//set the values of the text to nothing
 		_playerAnswer.setText("");
 		_correctAnswer.setText("");
+
+
 		_frameCounter.increaseCounter();
-		if(_frameCounter.getCounter()<10) {
+
+		//check if the counter value is above 10 if not then consider to move and change the labels
+		//if done then lock the buttons and move to the next frame
+		if (_frameCounter.getCounter() < 10) {
+
+
 			_correctnessImage.setImage(null);
-			
+
+			//locks the default buttons such as play , record
 			lockSomeBtns();
-			
-			WritableImage image=_Pane.snapshot(new SnapshotParameters(),null);
+
+			WritableImage image = _Pane.snapshot(new SnapshotParameters(), null);
 			_imageView1.setImage(image);
 			_imageView1.toFront();
-			
-			_num=_level.generateNumber();
+
+			//generate the number which is to be said by the player
+			_num = _level.generateNumber();
 			_label.setText(Integer.toString(_num));
-			TranslateTransition transition=new TranslateTransition(Duration.millis(500),_imageView1);
+
+			//translate transition
+			TranslateTransition transition = new TranslateTransition(Duration.millis(500), _imageView1);
 			transition.setByX(-1000f);
 			transition.setFromX(0);
-			transition.setOnFinished(new EventHandler<ActionEvent>(){
+			transition.setOnFinished(new EventHandler<ActionEvent>() {
 				@Override
 				public void handle(ActionEvent event) {
 					_imageView1.setImage(null);
 					transition.stop();
 				}
 			});
-			transition.playFromStart();	
-		}else {	
+			transition.playFromStart();
+
+		} else {
+
+			//get the values which the player got from playing
+			//the game
 			_player.setScore(_scoreCounter.getCounter());
 			_player.setAttempts(_attemptCounter.getCounter());
+
 			_player.setLevel(_level);
+
+			//this leads to the stats frame which shall show the values
+			//of the player
 			_mainApp.showStatsFrame(_player);
+
 		}
+
 
 	}
 
-
+	/**
+	 * handling of the back button which moves back to the previous frame
+	 * which is the level frame while reseting the stats of the players since
+	 * the player hasn't finished the game
+	 */
 	@FXML
 	public void handleBackButton() {
+
+		//resetting the players stats
 		_player.resetStats();
+
+		//show the level layout
 		_mainApp.showLevelLayout(_player);
+
 	}
 
+	/**
+	 * this handles the record button which shall start
+	 * the working of the SwingWorker thread which shall work the
+	 * htk methods in the background
+	 */
 	@FXML
 	public void handleRecordButton() {
+		//increase record counter
 		_recCounter.increaseCounter();
+
+		//text values to empty strings to erase prompt text
 		_correctAnswer.setText("");
 		_playerAnswer.setText("");
-		
+
+		//locks all the buttons
 		lockAllBtns();
-		
+
+		//deset any correct image graphics and start the thread
 		_correctnessImage.setImage(null);
+
+		//swingWorker methods
 		waitCursor();
-		DoWork dowork=new DoWork();
-		Thread thread=new Thread(dowork);
-		thread.start();	
-
-	}
-
-	@FXML
-	public void handleSubmitBtn() {
-		
-		_attemptCounter.increaseCounter();
-		
-		_nextButton.setDisable(false);
-		_playBtn.setDisable(true);
-		
-		Answer answer=new Answer();
-		boolean correctness=answer.checkAnswer(_num);
-		if(correctness) {
-			Image image=new Image(getClass().getClassLoader().getResource("resources/correct.png").toString(),true);
-			_correctnessImage.setImage(image);
-			
-			if(_recCounter.getCounter()==1) { 
-				_scoreCounter.increaseCounter();
-			}
-			String s=answer.getPLayerAnswer();
-			_playerAnswer.setText(s);
-			String correct=answer.getAnswer();
-			_correctAnswer.setText(correct);
-		}else {
-			Image image=new Image(getClass().getClassLoader().getResource("resources/wrong.png").toString(),true);
-			_correctnessImage.setImage(image);
-			String s=answer.getPLayerAnswer();
-			_playerAnswer.setText(s);
-
-		}
-		_submitBtn.setDisable(true);
-	}
-	
-	@FXML
-	public void handlePlayBtn() {
-
-		lockAllBtns();
-		waitCursor();
-		
-		PlayWorker playworker=new PlayWorker();
-		Thread thread=new Thread(playworker);
+		DoWork doWork = new DoWork();
+		Thread thread = new Thread(doWork);
 		thread.start();
 	}
-	
-	
-	public void setLevel(Level level) {
-		_level=level;
-		_num=_level.generateNumber();
-		_label.setText(Integer.toString(_num));
+
+	/**
+	 * the submit button is used to check the the voice recording that was attained
+	 * based on the string values with what the actual values of the string is
+	 */
+	@FXML
+	public void handleSubmitBtn() {
+		//increase attempt counter
+		_attemptCounter.increaseCounter();
+
+		//disable the other buttons
+		_nextButton.setDisable(false);
+		_playBtn.setDisable(true);
+
+		//checking the answer is correct
+		Answer answer = new Answer();
+		boolean correctness = answer.checkAnswer(_num);
+
+		if (correctness) {
+			Image image = new Image(getClass().getClassLoader().getResource("resources/correct.png").toString(), true);
+			_correctnessImage.setImage(image);
+
+			//checking if the recording has been acquired
+			if (_recCounter.getCounter() == 1) {
+				_scoreCounter.increaseCounter();
+			}
+
+			//gaining the string values and setting them to the textfields
+			//to show the player
+			String s = answer.getPLayerAnswer();
+			_playerAnswer.setText(s);
+
+			String correct = answer.getAnswer();
+			_correctAnswer.setText(correct);
+
+		} else {
+			//setting the wrong icon if the answer is incorrect
+			Image image = new Image(getClass().getClassLoader().getResource("resources/wrong.png").toString(), true);
+
+			_correctnessImage.setImage(image);
+			String s = answer.getPLayerAnswer();
+			_playerAnswer.setText(s);
+
+			//if the player is stuck on the same frame 3 times then it shall
+			//show the correct answer
+			if (_recCounter.getCounter() == 3) {
+				//get the correct answer
+				String correct = answer.getAnswer();
+				_correctAnswer.setText(correct);
+			}
+
+		}
 	}
 
+	/**
+	 * when the play button is pressed the voice that was captured for
+	 * recording is played to the user so they can check if they got the correct
+	 * answer
+	 */
+	@FXML
+	public void handlePlayBtn() {
+		//lock the buttons
+		lockAllBtns();
+
+		//swing Worker methods
+		waitCursor();
+		PlayWorker playWorker = new PlayWorker();
+		Thread thread = new Thread(playWorker);
+		thread.start();
+
+	}
+
+	//setter methods
+
+	/**
+	 * this method sets the level which is used to set the text based on the
+	 * easy (1-10) and hard (11 - 99)
+	 * @param level
+	 */
+	public void setLevel(Level level) {
+		_level = level;
+
+		//generating the number to set to the label
+		_num = level.generateNumber();
+		_label.setText(Integer.toString(_num));
+
+	}
+
+	/**
+	 * setting the MainApp.
+	 * @param mainApp
+	 */
 	public void setMainApp(MainApp mainApp){
 		_mainApp=mainApp;
 		normaliseCursor();
-	}	
+	}
 
+	/**
+	 * setting the player
+	 * @param player
+	 */
 	public void setPlayer(Player player) {
 		_player=player;
 	}
 	
 	
+	//idea we can use the popOver in the tutorial frame which i shall
+	//implement after getting the gameFrameController done
 	
-	
-	//need to be refactored
+	//need to be refactored -> split the to another method
+
+	/**
+	 * using the popOver which is from the controls fx library which basically
+	 * gives a pop up message plan to use for the tutorial screen
+	 */
 	public void showPopOver() {
 
 		Label label=new Label();
