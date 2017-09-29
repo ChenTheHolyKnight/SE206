135c583e25e65994eda195fd34cdc342e16f032d
Chen <czha959@aucklanduni.ac.nz>
2017-09-30 10:30:34 +1300

    write comments Level

--- src/assignment/model/EasyLevel.java
+++ src/assignment/model/EasyLevel.java
@@ -4,14 +4,22 @@ import java.util.Random;
 
 public class EasyLevel implements Level{
 	
+	//This is a field to store the indicator of this level
 	private Levels _levelStr=Levels.EASY;
 
+	
+	/**
+	 * This is a method to generate a number from 1 to 10
+	 */
 	@Override
 	public int generateNumber() {
 		Random rand =new Random();
 		return rand.nextInt(9)+1;
 	}
-
+	
+	/**
+	 * This is a method to get the level indicator
+	 */
 	@Override
 	public Levels getLevels() {		
 		return _levelStr;
--- src/assignment/model/Level.java
+++ src/assignment/model/Level.java
@@ -1,11 +1,20 @@
 package assignment.model;
 
 public interface Level {
-
+	/**
+	 * This is an enum to indicate the levels type
+	 */
 	public enum Levels{
 		EASY,HARD
 	}
 	
+	/**
+	 * method corresponding for generating a random number in each level
+	 */
 	public int generateNumber();
+	
+	/**
+	 * This is the method to get the enum indicator
+	 */
 	public Levels getLevels() ;
 }
